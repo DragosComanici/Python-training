3c1e269832ece12ccc11f284069393ca2e5fe83c
DragosComanici <dragos.comanici2@gmail.com>
2023-11-14 01:23:24 +0200

    added new stuff

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print('Hello world')
+#fuck you stefan for giving me homowork
+'''this is a string with "quotes" withing triple quotes'''
+age = 20
+name = 'bitch ass'
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python'.format(name))
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+i = 5
+i = i+1
+print(i)
