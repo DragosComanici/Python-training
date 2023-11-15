75a003352f52e0e216d28c580d6c1d25fdfe66a5
DragosComanici <dragos.comanici2@gmail.com>
2023-11-15 03:45:23 +0200

    new stuff

--- hello.py
+++ hello.py
@@ -11,3 +11,82 @@ print('c')
 i = 5
 i = i+1
 print(i)
+
+
+
+number = 23
+running = True
+
+while running:
+    guess = int(input('Enter an integer : '))
+
+    if guess == number:
+        print('Congratulations, you guessed it.')
+        # this causes the while loop to stop
+        running = False
+    elif guess < number:
+        print('No, it is a little higher than that.')
+    else:
+        print('No, it is a little lower than that.')
+else:
+    print('The while loop is over.')
+    # Do anything else you want to do here
+
+print('Done')
+
+
+#i dont really understand this one ("continue")
+while True:
+    s = input('Enter something : ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        print('Too small')
+        continue
+    print('Input is of sufficient length')
+    
+    
+    
+def print_max(a, b):
+    if a > b:
+        print(a, 'is maximum')
+    elif a == b:
+        print(a, 'is equal to', b)
+    else:
+        print(b, 'is maximum')
+
+# directly pass literal values
+print_max(3, 4)
+
+x = 5
+y = 7
+
+# pass variables as arguments
+print_max(x, y)
+
+
+
+def func(a, b=5, c=10):
+    print('a is', a, 'and b is', b, 'and c is', c)
+
+func(3, 7)
+func(25, c=24)
+func(c=50, a=100)
+
+
+#i dont understand
+def total(a=5, *numbers, **phonebook):
+    print('a', a)
+
+    #iterate through all the items in tuple
+    for single_item in numbers:
+        print('single_item', single_item)
+
+    #iterate through all the items in dictionary    
+    for first_part, second_part in phonebook.items():
+        print(first_part,second_part)
+
+total(10,1,2,3,Jack=1123,John=2231,Inge=1560)
+
+
+
